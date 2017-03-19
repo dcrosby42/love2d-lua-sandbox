cb55dafe2bbb09d5e0292a01c52dcfc4ab102dc0
David Crosby <dcrosby42@gmail.com>
2017-03-19 16:18:57 -0400

    hjkl keys to scan map

--- cat2d.love/modules/tiledtest.lua
+++ cat2d.love/modules/tiledtest.lua
@@ -12,17 +12,30 @@ M.newWorld = function()
   return world, nil
 end
 
-
+local transx=0
+local transy=0
+local spd = 64
+local Keys = {
+  h=function() transx = transx + spd end,
+  j=function() transy = transy - spd end,
+  k=function() transy = transy + spd end,
+  l=function() transx = transx - spd end,
+}
 M.updateWorld = function(world, action)
+  if action.state == 'pressed' then
+    local act = Keys[action.key]
+    if act then act() end
+  end
 end
 
 M.drawWorld = function(world)
   love.graphics.setBackgroundColor(unpack(world.bgcolor))
 
-  -- -- love.graphics.push()
+  love.graphics.push()
   -- love.graphics.scale(math.round(scale,2),math.round(scale,2))
+  love.graphics.translate(transx,transy)
   map:draw()
-  -- love.graphics.pop()
+  love.graphics.pop()
 end
 
 return M
