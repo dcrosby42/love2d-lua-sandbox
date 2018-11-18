5d2b854debb1a18dcaa78f8d8c67e172cebbdec3
David Crosby <dcrosby42@gmail.com>
2018-11-18 17:39:30 -0500

    cycling animals

--- cat2d.love/modules/animalscreen/init.lua
+++ cat2d.love/modules/animalscreen/init.lua
@@ -9,29 +9,31 @@ M.newWorld = function()
 
   local w = love.graphics.getWidth()
   local h = love.graphics.getHeight()
-  Debug.println("Bounds w="..w.." h="..h)
+  -- Debug.println("Bounds w="..w.." h="..h)
   return {
     stamps={},
+    selector=0,
   }
 end
 
 M.updateWorld = function(w,action)
   if action.type == "touch" or action.type == "mouse" then
     if action.state == "pressed" then
-      Debug.println("x")
-      local stamp = {x=action.x, y=action.y, animal=A.animals[1]}
+      local animal = A.animals[1+w.selector]
+      w.selector = (w.selector + 1) % #A.animals
+      local stamp = {x=action.x, y=action.y, animal=animal}
+      Debug.println("New ".. animal.name .. " @ " .. stamp.x .. "," .. stamp.y)
       table.insert(w.stamps, stamp)
+    end
+  elseif action.type == "keyboard" and action.state == "pressed" then
+    if action.key == "tab" then
+      w.stamps={}
     end
   end
 
   return w
 end
 
-local h = R.getImage("data/images/hippo.png")
-local offx=h:getWidth()/2
-local offy=h:getHeight()/2
-local sx=0.5
-local sy=0.5
 local r=0
 M.drawWorld = function(w)
   love.graphics.setBackgroundColor(0,0,0,0)
@@ -40,7 +42,7 @@ M.drawWorld = function(w)
     local img = R.getImage(a.file)
     offx=img:getWidth()*a.centerX
     offy=img:getHeight()*a.centerY
-    love.graphics.draw(h, stamp.x, stamp.y, r, a.sizeX, a.sizeY, offx, offy)
+    love.graphics.draw(img, stamp.x, stamp.y, r, a.sizeX, a.sizeY, offx, offy)
   end
   Debug.draw()
 end
